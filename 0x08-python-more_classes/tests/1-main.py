aadd8503101df874f94de9847c91dc1991d6300c
root <nouryhazem17@gmail.com>
2024-04-08 16:53:08 -0700

    1-rectangle.py

---
+++ 0x08-python-more_classes/tests/1-main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+Rectangle = __import__('1-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"1-main.py" [New File]                                            0,0-1         All
+:q
+
